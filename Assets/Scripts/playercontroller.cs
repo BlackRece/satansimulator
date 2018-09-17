15be4316b6ec5f301e950c583862222056b7512a
BlackRece <mauricemlth@hotmail.com>
2018-09-17 22:49:45 +0100

    Initial Upload of Unity Project

---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+    public GameObject player_obj;
+    //public GameObject camer_obj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //get sidewards movement
+        float x_player = Input.GetAxis("Vertical");
+        float z_player = Input.GetAxis("Horizontal");
+        
+        //get new pos
+        Vector3 newPos = player_obj.transform.position;
+        newPos.x += x_player;
+        newPos.z += z_player;
+
+        //move player
+        player_obj.transform.position = newPos;
+        //move camera
+        //Vector3 newCam = camer_obj.transform.position;
+        //newCam.x += x_player;
+        //camer_obj.transform.position = newCam;
+		
+	}
+}
